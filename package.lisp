44219452181872b5226c7f9d6b68781abbb7fdac
Lukáš Hrázký <lukkash@email.cz>
2025-06-01 12:27:58 +0200

    Drop the rewriting of the firmware app from lisp

--- package.lisp
+++ package.lisp
@@ -1,9 +1,4 @@
 (import "src/package_lib.bin" 'package-lib)
-
 (load-native-lib package-lib)
 
-; Switch Balance App to UART App
-(if (= (conf-get 'app-to-use) 9) (conf-set 'app-to-use 3))
-
-; Set firmware version:
 (apply ext-set-fw-version (sysinfo 'fw-ver))
