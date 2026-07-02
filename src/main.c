lukash/refloat

commit 21fb241bbae50460b1db31a9bf43b2153d4e3961

Fix LEDs On / Headlights On runtime toggles overwriting config
Fix: Fix toggling LEDs or Headlights On via the UI ok konami being
 stored in the config under some circumstances
 >
 The toggles are now only temprorary and once toggled, writing the
 config won't override the current state either.

src/main.c
@@ -178,8 +178,6 @@ static void reconfigure(Data *d) {
 static void configure(Data *d) {
     state_set_disabled(&d->state, d->float_conf.disabled);
 
-    lcm_configure(&d->lcm, &d->float_conf.leds);
-
     // Loop time in microseconds
     d->loop_time_us = 1e6 / d->float_conf.hertz;
 
@@ -214,6 +212,8 @@ static void configure(Data *d) {
 
     reconfigure(d);
 
+    lcm_configure(&d->lcm, &d->leds);
+
     if (d->state.state == STATE_DISABLED) {
         beep_alert(d, 3, false);
     } else if (d->state.state != STATE_STARTUP) {
@@ -221,9 +221,9 @@ static void configure(Data *d) {
     }
 }
 
-static void leds_headlights_switch(CfgLeds *cfg_leds, LcmData *lcm, bool headlights_on) {
-    cfg_leds->headlights_on = headlights_on;
-    lcm_configure(lcm, cfg_leds);
+static void leds_headlights_switch(Leds *leds, LcmData *lcm, bool headlights_on) {
+    leds_set_headlights_enabled(leds, headlights_on);
+    lcm_configure(lcm, leds);
 }
 
 static void reset_runtime_vars(Data *d) {
@@ -964,14 +964,15 @@ static void refloat_thd(void *arg) {
             }
 
             if (d->float_conf.hardware.leds.mode != LED_MODE_OFF) {
-                if (!d->leds.cfg->headlights_on &&
+                const LedsRuntimeStatus *led_status = leds_get_runtime_status(&d->leds);
+                if (!led_status->headlights_enabled &&
                     konami_check(&d->headlights_on_konami, &d->leds, &d->footpad, &d->time)) {
-                    leds_headlights_switch(&d->float_conf.leds, &d->lcm, true);
+                    leds_headlights_switch(&d->leds, &d->lcm, true);
                 }
 
-                if (d->leds.cfg->headlights_on &&
+                if (led_status->headlights_enabled &&
                     konami_check(&d->headlights_off_konami, &d->leds, &d->footpad, &d->time)) {
-                    leds_headlights_switch(&d->float_conf.leds, &d->lcm, false);
+                    leds_headlights_switch(&d->leds, &d->lcm, false);
                 }
             }
 
@@ -2028,7 +2029,7 @@ static void cmd_alerts_control(AlertTracker *at, uint8_t *buf, size_t len) {
     }
 }
 
-static void lights_control_request(CfgLeds *leds, uint8_t *buffer, size_t len, LcmData *lcm) {
+static void lights_control_request(Leds *leds, uint8_t *buffer, size_t len, LcmData *lcm) {
     if (len < 5) {
         return;
     }
@@ -2040,25 +2041,27 @@ static void lights_control_request(CfgLeds *leds, uint8_t *buffer, size_t len, L
         uint8_t value = buffer[ind++];
 
         if (mask & 0x1) {
-            leds->on = value & 0x1;
+            leds_set_enabled(leds, value & 0x1);
         }
 
         if (mask & 0x2) {
-            leds->headlights_on = value & 0x2;
+            leds_set_headlights_enabled(leds, value & 0x2);
         }
 
         lcm_configure(lcm, leds);
     }
 }
 
-static void lights_control_response(const CfgLeds *leds) {
+static void lights_control_response(const Leds *leds) {
     static const int bufsize = 3;
     uint8_t buffer[bufsize];
     int32_t ind = 0;
 
     buffer[ind++] = 101;  // Package ID
     buffer[ind++] = COMMAND_LIGHTS_CONTROL;
-    buffer[ind++] = leds->headlights_on << 1 | leds->on;
+
+    const LedsRuntimeStatus *status = leds_get_runtime_status(leds);
+    buffer[ind++] = status->headlights_enabled << 1 | status->enabled;
 
     SEND_APP_DATA(buffer, bufsize, ind);
 }
@@ -2273,8 +2276,8 @@ static void on_command_received(unsigned char *buffer, unsigned int len) {
         return;
     }
     case COMMAND_LIGHTS_CONTROL: {
-        lights_control_request(&d->float_conf.leds, &buffer[2], len - 2, &d->lcm);
-        lights_control_response(&d->float_conf.leds);
+        lights_control_request(&d->leds, &buffer[2], len - 2, &d->lcm);
+        lights_control_response(&d->leds);
         return;
     }
     case COMMAND_DATA_RECORD_REQUEST: {

src/lcm.c
@@ -34,17 +34,20 @@ void lcm_init(LcmData *lcm, CfgHwLeds *hw_cfg) {
     lcm->lights_off_when_lifted = true;
 }
 
-void lcm_configure(LcmData *lcm, const CfgLeds *cfg) {
+void lcm_configure(LcmData *lcm, const Leds *leds) {
     if (!lcm->enabled) {
         return;
     }
 
-    if (!cfg->on) {
+    const CfgLeds *cfg = leds->cfg;
+    const LedsRuntimeStatus *status = leds_get_runtime_status(leds);
+
+    if (!status->enabled) {
         lcm->brightness = 0.0f;
         lcm->brightness_idle = 0.0f;
         lcm->status_brightness = 0.0f;
     } else {
-        if (cfg->headlights_on) {
+        if (status->headlights_enabled) {
             lcm->brightness = cfg->headlights.brightness * 100;
             lcm->status_brightness = cfg->status.brightness_headlights_on * 100;
         } else {

src/lcm.h
@@ -18,6 +18,7 @@
 #pragma once
 
 #include "footpad_sensor.h"
+#include "leds.h"
 #include "motor_data.h"
 #include "state.h"
 
@@ -50,7 +51,7 @@ typedef struct {
 
 void lcm_init(LcmData *lcm, CfgHwLeds *hw_cfg);
 
-void lcm_configure(LcmData *lcm, const CfgLeds *cfg);
+void lcm_configure(LcmData *lcm, const Leds *leds);
 
 /**
  * Poll request from LCM with any data that need to be passed to the package.

src/leds.c
@@ -704,7 +704,7 @@ static void reset_led_bars(
 
 static bool headlights_should_be_on(const Leds *leds) {
     return (leds->state.state == STATE_RUNNING && leds->state.mode != MODE_FLYWHEEL) &&
-        leds->cfg->headlights_on;
+        leds->runtime_status.headlights_enabled;
 }
 
 static const LedBar *target_bar(const Leds *leds, bool flip) {
@@ -767,9 +767,14 @@ void leds_init(Leds *leds) {
     leds->status_on_front_idle_time = 0.0f;
     leds->board_is_upright = false;
 
-    leds->split_distance = 0.0f;
+    leds->runtime_status.enabled = false;
+    leds->runtime_status.headlights_enabled = false;
+    leds->runtime_status_overriden.enabled = false;
+    leds->runtime_status_overriden.headlights_enabled = false;
+
     leds->headlights_on = false;
     leds->direction_forward = true;
+    leds->split_distance = 0.0f;
     leds->headlights_time = 0.0f;
     leds->animation_start = 0;
 
@@ -872,9 +877,30 @@ void leds_configure(Leds *leds, const CfgLeds *cfg) {
     leds->headlights_trans.transition = cfg->headlights_transition;
     leds->dir_trans.transition = cfg->direction_transition;
 
+    if (!leds->runtime_status_overriden.enabled) {
+        leds->runtime_status.enabled = cfg->on;
+    }
+    if (!leds->runtime_status_overriden.headlights_enabled) {
+        leds->runtime_status.headlights_enabled = cfg->headlights_on;
+    }
+
     float current_time = VESC_IF->system_time();
     leds->status_idle_time = current_time;
     leds->status_on_front_idle_time = current_time;
+}
+
+const LedsRuntimeStatus *leds_get_runtime_status(const Leds *leds) {
+    return &leds->runtime_status;
+}
+
+void leds_set_enabled(Leds *leds, bool value) {
+    leds->runtime_status.enabled = value;
+    leds->runtime_status_overriden.enabled = true;
+}
+
+void leds_set_headlights_enabled(Leds *leds, bool value) {
+    leds->runtime_status.headlights_enabled = value;
+    leds->runtime_status_overriden.headlights_enabled = true;
 }
 
 void leds_update(Leds *leds, const State *state, FootpadSensorState fs_state) {
@@ -891,7 +917,7 @@ void leds_update(Leds *leds, const State *state, FootpadSensorState fs_state) {
         return;
     }
 
-    if (leds->cfg->on) {
+    if (leds->runtime_status.enabled) {
         if (leds->on_off_fade == 0.0f) {
             full_animation_reset(leds, current_time);
         }
@@ -951,7 +977,7 @@ void leds_update(Leds *leds, const State *state, FootpadSensorState fs_state) {
 
     // status brightness
     float status_brightness = leds->cfg->status.brightness_headlights_off;
-    if (leds->cfg->headlights_on) {
+    if (leds->runtime_status.headlights_enabled) {
         status_brightness = leds->cfg->status.brightness_headlights_on;
     }
     if (leds->status_idle_blend > 0.0f) {

src/leds.h
@@ -31,6 +31,11 @@ typedef struct {
 } TransitionState;
 
 typedef struct {
+    bool enabled;
+    bool headlights_enabled;
+} LedsRuntimeStatus;
+
+typedef struct {
     LedStrip status_strip;
     LedStrip front_strip;
     LedStrip rear_strip;
@@ -57,9 +62,13 @@ typedef struct {
     float status_on_front_idle_time;
     bool board_is_upright;
 
-    float split_distance;
+    LedsRuntimeStatus runtime_status;
+    // represents the state of a given flag being overriden at runtime
+    LedsRuntimeStatus runtime_status_overriden;
+
     bool headlights_on;
     bool direction_forward;
+    float split_distance;
     float headlights_time;
     float animation_start;
 
@@ -85,6 +94,12 @@ void leds_init(Leds *leds);
 void leds_setup(Leds *leds, CfgHwLeds *hw_cfg, const CfgLeds *cfg);
 
 void leds_configure(Leds *leds, const CfgLeds *cfg);
+
+const LedsRuntimeStatus *leds_get_runtime_status(const Leds *leds);
+
+void leds_set_enabled(Leds *leds, bool value);
+
+void leds_set_headlights_enabled(Leds *leds, bool value);
 
 void leds_update(Leds *leds, const State *state, FootpadSensorState fs_state);
 

commit d31ce1da4a5905cf595482235d55efe07e771795

Fix wrong initialization order for LCM
21fb241b broke the initialization for the LCM data. This reorders the
init calls so the data get initialized correctly.

Fix: Fix external (LCM) LEDs not turning on at startup

src/main.c
@@ -185,6 +185,8 @@ static void reconfigure(Data *d) {
     haptic_feedback_configure(&d->haptic_feedback, &d->float_conf);
     alert_tracker_configure(&d->alert_tracker, &d->float_conf);
 
+    leds_configure(&d->leds, &d->float_conf.leds);
+
     d->startup_pitch_trickmargin = d->float_conf.startup_dirtylandings_enabled ? 10 : 0;
     d->tiltback_variable =
         d->float_conf.tiltback_variable / 1000 * sign(d->float_conf.tiltback_variable_max);
@@ -1208,6 +1210,7 @@ static void data_init(Data *d) {
     reverse_stop_init(&d->reverse_stop);
 
     leds_init(&d->leds);
+    leds_setup(&d->leds, &d->float_conf.hardware.leds, &d->float_conf.leds);
     lcm_init(&d->lcm, &d->float_conf.hardware.leds);
     charging_init(&d->charging);
     bms_init(&d->bms);
@@ -2629,7 +2632,6 @@ static bool set_cfg(uint8_t *buffer) {
     if (res) {
         write_cfg_to_eeprom(d);
         configure(d);
-        leds_configure(&d->leds, &d->float_conf.leds);
     }
 
     return res;
@@ -2700,7 +2702,6 @@ INIT_FUN(lib_info *info) {
     }
 
     footpad_sensor_update(&d->footpad, &d->float_conf);
-    leds_setup(&d->leds, &d->float_conf.hardware.leds, &d->float_conf.leds);
 
     VESC_IF->imu_set_read_callback(imu_ref_callback);
     VESC_IF->conf_custom_add_config(get_cfg, set_cfg, get_cfg_xml);